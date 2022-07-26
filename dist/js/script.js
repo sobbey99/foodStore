0f55bb05074a5de249414d922c5700a1645b80af
Mihail <sobbey99@gmail.com>
2022-07-26 11:36:01 -0600

    Made language dropdown in header for PC

--- dist/js/script.js
+++ dist/js/script.js
@@ -42,9 +42,25 @@ function image_loaded() {
 
 // END PRELOADER
 
+//HEADER LANGUAGE DROPDOWN - START
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
 
-
-
-// START HEADER
-// xxxx
-// END HEADER
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.header-dropdown__btn')) {
+  
+      var dropdowns = document.getElementsByClassName("header-dropdown__content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  };
+//HEADER LANGUAGE DROPDOWN - END
